8ab399d3941ea25acf1075fb3d448614fbe19d22
Leonardo Magri da Costa <166085305+LeoMagri@users.noreply.github.com>
2026-01-25 10:58:58 -0400

    Update PreparadorDadosBI.java

--- PreparadorDadosBI.java
+++ PreparadorDadosBI.java
@@ -2,24 +2,31 @@ public class PreparadorDadosBI {
     public static void main(String[] args) {
         System.out.println("--- Iniciando Processamento para Power BI ---");
 
-        // Simulacao de dados que viriam de um SQL no SGU
+        // Simulação de dados que viriam de um SQL no SGU (Números de faturas e seus respectivos status)
         double[] faturas = {22345682, 22345684, 22345689, 22325682, 21345692};
         String[] status = {"INTEGRADA", "PENDENTE", "INTEGRADA", "INTEGRADA", "NAO INTEGRADA"};
 
-        double totalParaDashboard = 0;
+        double totalIntegrada = 0;
+        double totalPendente = 0;
         int faturasContabilizadas = 0;
 
         // Lógica de Filtro (O que o BI vai consumir)
         for (int i = 0; i < faturas.length; i++) {
             if (status[i].equals("INTEGRADA")) {
-                totalParaDashboard += faturas[i];
+                totalIntegrada += faturas[i];
                 faturasContabilizadas++;
-                System.out.println("Fatura " + (i+1) + ": R$ " + faturas[i] + " [Enviada para o BI]");
+                System.out.println("Fatura " + (i+1) + ": Status INTEGRADA - Processada.");
+            }
+            else if (status[i]. equals("PENDENTE")) {
+                totalPendente += faturas[i]
+                    faturasContabilizadas++;
+                System.out.println("Fatura " + (i+1) + ": Status PENDENTE - Enviada para revisão.");
             }
         }
 
         System.out.println("\n--- Resumo Final ---");
-        System.out.println("Total Processado: R$ " + totalParaDashboard);
-        System.out.println("Qtd Faturas no Gráfico: " + faturasContabilizadas);
+        System.out.println("Total Integrado: R$ " + totalParaDashboard);
+        System.out.println("Total Pendentes: R$ " + totalPendente);
+        System.out.println("Qtd Total de Faturas no Gráfico: " + faturasContabilizadas);
     }
 }
