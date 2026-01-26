198565e9a6d5f2265f5bcf3a9c48124bcd12301c
Leonardo Magri da Costa <166085305+LeoMagri@users.noreply.github.com>
2026-01-26 08:52:30 -0400

    Update queries_auditoria.sql

--- monitor-auditoria-tiss/queries_auditoria.sql
+++ monitor-auditoria-tiss/queries_auditoria.sql
@@ -1,1 +1,6 @@
-
+-- Filtragem para localizar no banco de dados quais os itens cadastrados no grupo de validacao utilizados para regras de grupo de procedimentos --
+SELECT SGI.CD_GRUPO, SG.NM_GRUPO, SGI.CD_ITEM, SCI.DS_ITEM
+  FROM BASEPROC.SCE_GRUPOS_ITENS sgi, BASEPROC.SCE_CFG_ITENS sci, BASEPROC.SCE_GRUPOS sg
+WHERE SGI.CD_ITEM = SCI.CD_ITEM
+   AND SGI.CD_GRUPO = 156200025404
+   AND SGI.CD_GRUPO = SG.CD_GRUPO
